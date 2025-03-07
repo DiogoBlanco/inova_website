f61c4cf20e059a99b796d902945dfce8ed926e97
DiogoBlanco <diogoblanco@outlook.com.br>
2025-03-06 22:04:11 -0300

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Inova Smart Home",
@@ -7,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
